82d9dc1bf422a82b2ded99abd20abb6221265fdf
App Generator <51070104+app-generator@users.noreply.github.com>
2025-09-22 04:54:17 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class To-Do(models.Model):
+
+    #__To-Do_FIELDS__
+    description = models.TextField(max_length=255, null=True, blank=True)
+    date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    #__To-Do_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("To-Do")
+        verbose_name_plural = _("To-Do")
+
+
+
+#__MODELS__END
